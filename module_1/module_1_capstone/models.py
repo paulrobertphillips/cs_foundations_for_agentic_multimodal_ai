42430cbed5525c3328ee0b512a664347095cf549
Paul Phillips <paul.rob.phillips@gmail.com>
2025-12-15 08:18:59 -0500

    added comments to models.py, since this wasn't actually used in solution.

--- module_1/module_1_capstone/models.py
+++ module_1/module_1_capstone/models.py
@@ -1,6 +1,13 @@
 from dataclasses import dataclass
 from typing import Dict, List
 
+"""
+Not actually used in this capstone project,
+but idea would be to have returned dictionaries
+from planning & executing tasks stored in class
+object.
+"""
+
 @dataclass
 class AgentRunResult:
     execution_order: List[str]
